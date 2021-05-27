f8a6379a0fff29cc881a88a5bef33589e36e9357
goodjobguy1234 <goodjobguy1234@gmail.com>
2021-05-27 16:24:42 +0700

    update

---
+++ app.py
@@ -0,0 +1,58 @@
+import time
+from tkinter import *
+import PIL
+import cv2
+from PIL import ImageTk, Image
+
+class app:
+    def __int__(self,window,window_title):
+        self.window = window
+        self.window.title(window_title)
+        self.video_source = 0
+        self.vid = my_video_capture(self.video_source)
+        self.canvas = Canvas(window, width=self.vid.width, height=self.vid.height)
+        self.canvas.pack()
+        self.btn_snapshot = Button(window, text="Snapshot", width=50, command=self.snapshot)
+        self.btn_snapshot.pack(anchor=CENTER, expand=True)
+        self.delay = 15
+        self.update()
+        self.window.mainloop()
+
+    def snapshot(self):
+        ret, frame = self.vid.get_frame()
+        if ret:
+            cv2.imwrite("frame-" + time.strftime("%d-%m-%Y-%H-%M-%S") + ".jpg",
+                        cv2.cvtColor(frame, cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)))
+
+    def update(self):
+        ret, frame = self.vid.get_frame()
+        if ret:
+            self.photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(frame))
+            self.canvas.create_image(0, 0, image=self.photo, anchor=NW)
+        self.window.after(self.delay, self.update)
+
+class my_video_capture:
+    def __init__(self,video_source=0):
+        self.vid = cv2.VideoCapture(video_source)
+        self.width = self.vid.get(cv2.CAP_PROP_FRAME_WIDTH)
+        self.height = self.vid.get(cv2.CAP_PROP_FRAME_HEIGHT)
+
+    def get_frame(self):
+        if self.vid.isOpened():
+            ret, frame = self.vid.read()
+            if ret:
+                return ret, cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            else:
+                return ret, None
+        else:
+            return False, None
+
+    def __del__(self):
+        if self.vid.isOpened():
+            self.vid.release()
+
+
+if __name__ == '__main__':
+    root = Tk()
+    title = "myapp"
+    app(root,title)
--- main.py
+++ main.py
@@ -1,20 +1,22 @@
+import time
+from tkinter import *
+from PIL import Image, ImageTk
 import face_recognition as face
 import cv2
-import dlib
-import numpy as np
-from PIL import Image, ImageDraw
+
 
 video_capture = cv2.VideoCapture(0)
 color = (0, 255, 0)
-thickness = 2
-process = True
+thickness = 1
+multiply = 2
+
 
 def draweye(mark):
     for i in range(0, len(mark)):
         p1_x, p2_x = mark[i - 1]
         p1_y, p2_y = mark[i]
-        x = (p1_x * 2, p2_x * 2)
-        y = (p1_y * 2, p2_y * 2)
+        x = (p1_x * multiply, p2_x * multiply)
+        y = (p1_y * multiply, p2_y * multiply)
         cv2.line(frame, y, x, color, thickness)
 
 
@@ -22,13 +24,15 @@ def drawface(mark):
     for i in range(1, len(mark)):
         p1_x, p2_x = mark[i - 1]
         p1_y, p2_y = mark[i]
-        x = (p1_x * 2, p2_x * 2)
-        y = (p1_y * 2, p2_y * 2)
+        x = (p1_x * multiply, p2_x * multiply)
+        y = (p1_y * multiply, p2_y * multiply)
         cv2.line(frame, y, x, color, thickness)
+
 
 def plot(mark):
     for (x, y) in mark:
-        cv2.circle(frame, (x * 2, y * 2), 2, color, -1)
+        cv2.circle(frame, (x * multiply, y * multiply), 2, color, -1)
+
 
 def is_between(nose):
     global color
@@ -38,44 +42,31 @@ def is_between(nose):
     p31_x = nose[3][0]
 
     if p28_x == p29_x == p30_x == p31_x:
-        color = (0,255, 0)
+        color = (0, 255, 0)
     else:
-        color = (0,0,255)
+        color = (0, 0, 255)
 
-while True:
+
+while video_capture.isOpened():
     ret, frame = video_capture.read()
     small_frame = cv2.resize(frame, (0, 0), fx=0.5, fy=0.5)
     rgb_small_frame = small_frame[:, :, ::-1]
 
-    if process:
-        face_landmarks_list = face.face_landmarks(rgb_small_frame)
+    face_landmarks_list = face.face_landmarks(rgb_small_frame)
+    for face_landmarks in face_landmarks_list:
 
-        for face_landmarks in face_landmarks_list:
+        for facial_feature in face_landmarks.keys():
+            mark = face_landmarks[facial_feature]
+            nose = face_landmarks['nose_bridge']
 
-            for facial_feature in face_landmarks.keys():
+            is_between(nose)
 
-                # print("The {} in this face has the following points: {}".format(facial_feature,
-                #                                                                 face_landmarks[facial_feature]))
-                # for point in face_landmarks[facial_feature]:
-                #     print(point)
-                #     cv2.circle(frame, point, 1, color, -1)
-
-                # for i in range(1 ,len(face_landmarks[facial_feature])):
-                #     p1 = face_landmarks[facial_feature][i-1]
-                #     p2 = face_landmarks[facial_feature][i]
-                #     cv2.line(frame, p1, p2, color, 2)
-                mark = face_landmarks[facial_feature]
-                nose = face_landmarks['nose_bridge']
-
-                is_between(nose)
-
-                plot(mark)
-                if facial_feature != 'left_eye' and facial_feature != 'right_eye':
-                    drawface(mark)
-                else:
-                    draweye(mark)
-            cv2.imshow('vdo', frame)
-    process = not process
+            plot(mark)
+            if facial_feature != 'left_eye' and facial_feature != 'right_eye':
+                drawface(mark)
+            else:
+                draweye(mark)
+    cv2.imshow('vdo', frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
